6d6ca5499ab7fabb540c98c58ad4f7290a9f3996
Emir-de-la-moquette <shanky.yt@gmail.com>
2025-09-15 14:34:44 +0200

    fonction sub

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,16 +6,23 @@ import os
 sys.path.append(os.getcwd())
 
 from src.greeting.main import greet # pylint: disable=wrong-import-position
-from src.greeting.utils import add # pylint: disable=wrong-import-position
+from src.greeting.utils import add, sub # pylint: disable=wrong-import-position
 
 
 def test_greet():
-    """_summary_
+    """test 1
     """
     assert greet("World") == "Hello, World!"
 
 
 def test_add():
-    """_summary_
+    """test 2
     """
     assert add(5, 7) == 12
+
+
+def test_sub():
+    """test 3
+    """
+    assert sub(69, 32) == 37
+
